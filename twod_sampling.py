0a57c823d578743c00a939522fa1d12c8f70af39
Om Sharan Salafia <om.salafia@inaf.it>
2021-04-01 21:44:22 +0200

    Add alternative, simpler monte carlo method

--- twod_sampling.py
+++ twod_sampling.py
@@ -3,7 +3,7 @@ from scipy.interpolate import LinearNDInterpolator
 import emcee
 
 
-def sample2D(X,Y,d2P_dXdY,N):
+def sample2D(X,Y,d2P_dXdY,N,method='simple'):
     """
     Extract samples from a 2D probability distribution.
     
@@ -12,6 +12,9 @@ def sample2D(X,Y,d2P_dXdY,N):
     - Y: y-axis grid over which the probability has been evaluated (1D numpy array of length m or 2D array of shape [m,n])
     - d2P_dXdY: 2D probability density (2D array of shape [m,n])
     - N: number of samples desired (int)
+    
+    Keywords:
+    - method: either 'MCMC' or 'simple'
     
     Returns: X_i, Y_i
     - X_i, Y_i: 1D arrays of length N containing samples that follow the given 2D probability distribution
@@ -47,51 +50,65 @@ def sample2D(X,Y,d2P_dXdY,N):
     plt.show()
     """
     
-    # define logprob function to be used in emcee
-    loglike = lambda coords: I(coords[0],coords[1])
+    if method=='MCMC':
+        
+        # define logprob function to be used in emcee
+        loglike = lambda coords: I(coords[0],coords[1])
+        
+        # setup emcee sampler
+        ndim = 2
+        nwalkers = 4*ndim
+        nsteps = int(1e7)
+        
+        # uniform initial conditions
+        x0s = np.random.uniform(x.min(),x.max(),nwalkers)
+        y0s = np.random.uniform(y.min(),y.max(),nwalkers)
+        
+        p0s = np.vstack([x0s,y0s]).T
+        
     
-    # setup emcee sampler
-    ndim = 2
-    nwalkers = 4*ndim
-    nsteps = 10*N
+        print(p0s.shape)
+        
+        # instantiate sampler
+        sampler = emcee.EnsembleSampler(nwalkers,ndim,loglike)
+        
+        # We'll track how the average autocorrelation time estimate changes
+        index = 0
+        autocorr = np.empty(nsteps)
+        
+        # This will be useful to testing convergence
+        old_tau = np.inf
+        
+        for sample in sampler.sample(p0s, iterations=nsteps, progress=True):
+            # Only check convergence every 100 steps
+            if sampler.iteration % 100:
+                continue
+        
+            # Compute the autocorrelation time so far
+            # Using tol=0 means that we'll always get an estimate even
+            # if it isn't trustworthy
+            tau = sampler.get_autocorr_time(tol=0)
+            autocorr[index] = np.mean(tau)
+            index += 1
+        
+            # Check convergence
+            converged = np.all(tau * 100 < sampler.iteration)
+            converged &= np.all(np.abs(old_tau - tau) / tau < 0.1)
+            if converged and (len(sampler.flatchain)>(2*N)): # require the chain to be at least 2*N long
+                break
+            old_tau = tau
+        
+        samples = sampler.flatchain[sampler.flatchain.shape[0]-int(N):,:]
     
-    # uniform initial conditions
-    x0s = np.random.uniform(x.min(),x.max(),nwalkers)
-    y0s = np.random.uniform(y.min(),y.max(),nwalkers)
-    
-    p0s = np.vstack([x0s,y0s]).T
-    print(p0s.shape)
-    
-    # instantiate sampler
-    sampler = emcee.EnsembleSampler(nwalkers,ndim,loglike)
-    
-    # We'll track how the average autocorrelation time estimate changes
-    index = 0
-    autocorr = np.empty(nsteps)
-    
-    # This will be useful to testing convergence
-    old_tau = np.inf
-    
-    for sample in sampler.sample(p0s, iterations=nsteps, progress=True):
-        # Only check convergence every 100 steps
-        if sampler.iteration % 10:
-            continue
-    
-        # Compute the autocorrelation time so far
-        # Using tol=0 means that we'll always get an estimate even
-        # if it isn't trustworthy
-        tau = sampler.get_autocorr_time(tol=0)
-        autocorr[index] = np.mean(tau)
-        index += 1
-    
-        # Check convergence
-        converged = np.all(tau * 100 < sampler.iteration)
-        converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
-        if converged and (sampler.iteration>(2*N/8)): # require the chain to be at least 2*N long
-            break
-        old_tau = tau
-    
-    samples = sampler.flatchain[sampler.flatchain.shape[0]-int(N):,:]
+    else:
+        
+        x0s = np.random.uniform(x.min(),x.max(),N*10)
+        y0s = np.random.uniform(y.min(),y.max(),N*10)
+        z0s = np.random.uniform(0.,z.max(),N*10)
+        
+        good = I(x0s,y0s)>=np.log(z0s)
+        
+        samples = np.vstack([x0s[good],y0s[good]]).T
     
     return samples
     
